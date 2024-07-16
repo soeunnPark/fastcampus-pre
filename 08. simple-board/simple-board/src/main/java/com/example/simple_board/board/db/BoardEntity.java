ef14d4853563ac3af4c65266f3db628dcb3fc1f1
carol <carol072400@gmail.com>
2024-07-17 01:17:37 +0900

    refacor: Ch03 어노테이션 라이브러리 변경

--- 08. simple-board/simple-board/src/main/java/com/example/simple_board/board/db/BoardEntity.java
+++ 08. simple-board/simple-board/src/main/java/com/example/simple_board/board/db/BoardEntity.java
@@ -4,8 +4,8 @@ package com.example.simple_board.board.db;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.*;
-import org.springframework.data.annotation.Id;
 
 @Getter
 @Setter
